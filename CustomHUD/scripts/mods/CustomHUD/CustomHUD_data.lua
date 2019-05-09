7f42324e1ed3de8701df7bd71b2a2597de94d189
Azumgi <4zumgi@gmail.com>
2019-05-09 12:13:38 +0300

    [CustomHUD] Update for the latest VMF API

--- CustomHUD/scripts/mods/CustomHUD/CustomHUD_data.lua
+++ CustomHUD/scripts/mods/CustomHUD/CustomHUD_data.lua
@@ -20,55 +20,37 @@ local mod_data = {
 	allow_rehooking = true,
 }
 
-mod_data.options_widgets = {
-	{
-		["setting_name"] = SETTING_NAMES.CUSTOM_HUD,
-		["widget_type"] = "dropdown",
-		["text"] = "HUD Preset",
-		["tooltip"] = "Choose between different HUDs:\n" ..
-			"Default HUD:\n" ..
-			"Vanilla game HUD.\n" ..
-			"\n" ..
-			"Console HUD:\n" ..
-			"A different HUD is used when you play the game with a gamepad, one which has a larger health " ..
-			"bar but is more compact overall. Enabling this option will cause the gamepad HUD to be used " ..
-			"even when you are not using a gamepad.\n" ..
-			"\n" ..
-			"Custom HUD:\n" ..
-			"Minimalistic, screen real estate saving HUD.",
-		["options"] = {
-			{text = "Default HUD", value = mod.OFF},
-			{text = "Console HUD", value = mod.GAMEPAD},
-			{text = "Custom HUD", value = mod.CUSTOM},
-		},
-		["default_value"] = mod.CUSTOM,
-		["sub_widgets"] = {
-			{
-				["show_widget_condition"] = { mod.CUSTOM },
-				["setting_name"] = SETTING_NAMES.COLORIZE_AMMO,
-				["widget_type"] = "checkbox",
-				["text"] = "Change Ammo Counter Color",
-				["tooltip"] = "Changes text color of the ammo counter to red at 0 ammo.",
-				["default_value"] = false,
+mod_data.options = {
+	widgets = {
+		{
+			setting_id    = SETTING_NAMES.CUSTOM_HUD,
+			type          = "dropdown",
+			default_value = mod.CUSTOM,
+			options = {
+				{text = "default_hud_option", value = mod.OFF},
+				{text = "console_hud_option", value = mod.GAMEPAD},
+				{text = "custom_hud_option",  value = mod.CUSTOM, show_widgets = {1, 2, 3}},
 			},
-			{
-				["show_widget_condition"] = { mod.CUSTOM },
-				["setting_name"] = SETTING_NAMES.COLORIZE_SLOTS,
-				["widget_type"] = "checkbox",
-				["text"] = "Highlight Party Slots",
-				["tooltip"] = "Highlight item slots of players with effect sharing trinkets when you have a shareable item.",
-				["default_value"] = false,
-			},
-			{
-				["show_widget_condition"] = { mod.CUSTOM },
-				["setting_name"] = SETTING_NAMES.GRAYSCALE,
-				["widget_type"] = "checkbox",
-				["text"] = "Use Grayscale Icons",
-				["tooltip"] = "Will use the grayscale version icons for consumables.",
-				["default_value"] = false,
+			sub_widgets = {
+				{
+					setting_id    = SETTING_NAMES.COLORIZE_AMMO,
+					type          = "checkbox",
+					default_value = false,
+				},
+				{
+					setting_id    = SETTING_NAMES.COLORIZE_SLOTS,
+					type          = "checkbox",
+					default_value = false,
+				},
+				{
+					setting_id    = SETTING_NAMES.GRAYSCALE,
+					type          = "checkbox",
+					default_value = false,
+				},
 			},
 		},
-	},
+	}
 }
+
 
 return mod_data
